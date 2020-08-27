b56abe5687e9d18748b61edb1200df3838c3c285
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-08-27 17:45:00 -0500

    finished project

--- buggy.cpp
+++ buggy.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
  //Blank A
+#include <vector>
+using namespace std;
+
 class node {
  //Blank B
- 	int val;
- 	node* next;
+  public:
+   	int val;
+   	node* next;
 };
 
 void create_LL(vector<node*>& mylist, int node_num){
@@ -12,21 +16,22 @@ void create_LL(vector<node*>& mylist, int node_num){
     //create a set of nodes
     for (int i = 0; i < node_num; i++) {
         //Blank C
-        mylist[i].val = i;
-        mylist[i].next = NULL;
+        mylist[i] = new node;
+        mylist[i]->val = i;
+        mylist[i]->next = NULL;
     }
 
     //create a linked list
-    for (int i = 0; i < node_num; i++) {
-        mylist[i].next = mylist[i+1];
+    for (int i = 0; i < node_num - 1; i++) {
+        mylist[i]->next = mylist[i+1];
     }
 }
 
 int sum_LL(node* ptr) {
     int ret = 0;
     while(ptr) {
-        ret += ptr.val;
-        ptr = ptr.next;
+        ret += ptr->val;
+        ptr = ptr->next;
     }
     return ret;
 }
@@ -41,4 +46,7 @@ int main(int argc, char ** argv){
 
     //Step4: delete nodes
     //Blank D
+    for (int i = 0; i < NODE_NUM; i++) {
+        delete mylist[i];
+    }
 }
